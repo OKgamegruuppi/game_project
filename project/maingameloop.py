6f8cb66dae8caacd3d132c7d1cd7b2ca1b7f0a15
CoolCodeMan <mmssposti94@gmail.com>
2023-09-21 15:49:29 +0300

    edit

--- project/maingameloop.py
+++ project/maingameloop.py
@@ -25,12 +25,8 @@ class Mainloop():
 #Refreshing the screen.
     def draw_screen(self):
         self.display.fill((250, 250, 250))
-
         #screenfresh.py creates/fetches objects
-        #
-        #             
         #THIS UPDATES
-        
         pygame.display.flip()
         self.clock.tick(60)
 
