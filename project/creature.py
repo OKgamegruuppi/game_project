fa84775fd761721a1b67b10bac33037c0fdede70
aogasd <144658261+aogasd@users.noreply.github.com>
2023-09-21 10:02:26 +0300

    Update creature.py

--- project/creature.py
+++ project/creature.py
@@ -1,2 +1,15 @@
-class Creature(self,name,health,speed,direction=[x,y]):
+class Creature():
+    def __init__(self,name,pos=(x,y),dir=(x,y),health,speed):
+        self.name = name
+        self.health = health
+        self.speed = speed
+        self.dir = dir          #direction
+        self.pos = pos          #position
+
+    def movement():
+        pass
+
+    def hp_change():
+        pass
+
     pass
