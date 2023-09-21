89d5eccc944ba0edef249bc2eb2789ea7c032b38
CoolCodeMan <81040054+CoolCodeMan@users.noreply.github.com>
2023-09-21 10:35:42 +0300

    Update screenrefresher.py

--- project/screenrefresher.py
+++ project/screenrefresher.py
@@ -2,13 +2,13 @@
 
 def refreshdisplay(self):
   print(".blit objects onto the screen")
-  self.display.fill((250, 250, 250))
+  self.display.fill((250, 250, 250)) #täyttää näytön valkoisella, RBG koodit.
 
 
 
   
-  pygame.display.flip()
-  self.clock.tick(60)
+  pygame.display.flip() #näytön päivitys
+  self.clock.tick(60) # näytön päivitys
 
 
 
