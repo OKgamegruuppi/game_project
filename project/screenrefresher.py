4d2510910bac8826b7dbf3c5be8294333eadbc82
CoolCodeMan <81040054+CoolCodeMan@users.noreply.github.com>
2023-09-21 10:34:27 +0300

    Update screenrefresher.py

--- project/screenrefresher.py
+++ project/screenrefresher.py
@@ -1,8 +1,14 @@
 
 
-def refreshdisplay():
+def refreshdisplay(self):
   print(".blit objects onto the screen")
+  self.display.fill((250, 250, 250))
 
+
+
+  
+  pygame.display.flip()
+  self.clock.tick(60)
 
 
 
