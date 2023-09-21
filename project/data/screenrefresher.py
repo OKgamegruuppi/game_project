2bfbd3515060360e4e2bc336aa12e397a97b4bc2
CoolCodeMan <mmssposti94@gmail.com>
2023-09-21 13:50:43 +0300

    edits

--- project/data/screenrefresher.py
+++ project/data/screenrefresher.py
@@ -2,8 +2,6 @@ import pygame
 
 def draw_screen(self):
     self.display.fill((250, 250, 250))
-    #bg = pygame.image.load(main_game_loop.background_image)
-    #self.display.blit()
 
     pygame.display.flip()
     self.clock.tick(60)
