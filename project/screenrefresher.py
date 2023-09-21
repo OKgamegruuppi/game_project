5268c6477a422cedee71397d6694e9bc81257f20
CoolCodeMan <81040054+CoolCodeMan@users.noreply.github.com>
2023-09-21 11:48:09 +0300

    Update screenrefresher.py

--- project/screenrefresher.py
+++ project/screenrefresher.py
@@ -4,7 +4,11 @@ def refreshdisplay(self):
   print(".blit objects onto the screen")
   self.display.fill((250, 250, 250)) #fills the screen with white, RBG
 
+  #PLAYER POSITION
 
+  #ENEMY POSITION
+
+  #CURRENT BACKGROUND 
   
   pygame.display.flip() # refreshing the display
   self.clock.tick(60) # refreshing the display
