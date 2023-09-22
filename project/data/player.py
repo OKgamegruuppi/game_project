bf9e9621f2fed68971d8c84113dec177aa8e5670
VeliNalle <kjk861@gmail.com>
2023-09-22 12:09:33 +0300

    movement + hitbox version 1

--- project/data/player.py
+++ project/data/player.py
@@ -1,9 +1,11 @@
+import pygame
 from data.creature import Creature
 
 class Player(Creature):
     def __init__(self,name,icon,hitbox,pos_x,pos_y,dir,speed=5,health=10,target=None,status={},awareness=0,dmg=1):
         super().__init__(name,icon,hitbox,pos_x,pos_y,dir,speed,health,target,status,awareness)     
         self.dmg = dmg
+        self.hitbox = pygame.Rect(self.pos_x,self.pos_y,hitbox,hitbox)
         # Determine if player is moving in any direction
         self.move = {
             "up" : False,
@@ -18,12 +20,16 @@ class Player(Creature):
     def movement(self):
         if self.move["right"] == True:
             self.pos_x += self.speed
+            self.hitbox = self.hitbox.move(self.speed,0)
         if self.move["left"] == True:
             self.pos_x -= self.speed
+            self.hitbox = self.hitbox.move(-self.speed,0)
         if self.move["up"] == True:
-            self.pos_y += self.speed
-        if self.move["down"] == True:
             self.pos_y -= self.speed
+            self.hitbox = self.hitbox.move(0,-self.speed)
+        if self.move["down"] == True:
+            self.pos_y += self.speed
+            self.hitbox = self.hitbox.move(0,self.speed)
 
     def attack(self,target):
         pass
@@ -34,6 +40,7 @@ class Player(Creature):
     def interact(self,target=None):
         print("Nothing to interact with!")
     
-    def process(self,screen):
+    def update(self,screen):
         self.movement()
+        pygame.draw.rect(screen,"#333333",self.hitbox)
         screen.blit(self.icon,(self.pos_x,self.pos_y))
