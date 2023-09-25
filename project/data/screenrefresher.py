fcfac045daa9bed4e4ec04a81262fe2181ba5cb2
CoolCodeMan <81040054+CoolCodeMan@users.noreply.github.com>
2023-09-25 09:25:00 +0300

    Update screenrefresher.py

--- project/data/screenrefresher.py
+++ project/data/screenrefresher.py
@@ -1,6 +1,10 @@
 import pygame
 
 def draw_on_screen(self):
-    
-    print("draw_on_screen function")
-
+    self.display.fill((250, 250, 250))
+    self.display.blit(self.background, (300, 300))
+    self.objects.update(self.objects2)
+    self.objects.draw(self.display)
+    self.objects2.update()
+    self.objects2.draw(self.display)
+    pygame.display.flip()
