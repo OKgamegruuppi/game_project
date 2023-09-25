d068f572e9d2a60101c6fa58ca497f70a66b4073
aogasd <144658261+aogasd@users.noreply.github.com>
2023-09-25 13:13:42 +0300

    working on hitboxes

--- project/data/screenrefresher.py
+++ project/data/screenrefresher.py
@@ -5,8 +5,10 @@ import pygame
 def draw_on_screen(self):
     self.display.fill((250, 250, 250))
     self.display.blit(self.background, (300, 300))
+    self.borders.draw(self.display)
     self.playergroup.update(self.enemies)
     self.playergroup.draw(self.display)
-    self.enemies.update()
+    self.enemies.update(self.borders,self.playergroup)
     self.enemies.draw(self.display)
+
     pygame.display.flip()
--- project/maingameloop.py
+++ project/maingameloop.py
@@ -1,6 +1,7 @@
 #Necessary libraries
 from data.settings import windowsizeX, windowsizeY
 from data.creature import Creature, Enemy
+from data.location import Map_object,borders
 from data.player import Player
 from data.controls import game_event_observer
 from data.screenrefresher import draw_on_screen
@@ -18,18 +19,22 @@ class Mainloop():
         pygame.display.set_caption("GAME WINDOW")
 
         # Load test images
-        marcos = pygame.image.load("data/assets/slime_monster.png")
+        marcos = pygame.image.load("data/assets/slime_monster_mid.png")
         cat1_ico = pygame.image.load("data/assets/Cat-sprite-stand.png")
         defaultEnemy_icon2 = pygame.image.load("data/assets/TEST_Light_balls_tree1.png")
         # Make objects, and add them to the Group    
         ##(name,image,pos_x,pos_y,dir,speed,health=0,target=None,status={},awareness=0)
 
-        self.player = Player("Marcos Petriades",marcos,0,0,(0,0))
+        self.player = Player("Marcos Petriades",marcos,20,20,(0,0))
         cat1 = Creature("Cat",cat1_ico,400,300,[1,0],1,1,None,{"walking":40,"standing":0})
+        cat2 = Creature("Cat",cat1_ico,500,300,[1,0],2,1,None,{"walking":60,"standing":0})
+        cat3 = Creature("Cat",cat1_ico,500,300,[1,0],3,1,None,{"walking":60,"standing":0})
         defaultEnemy2 = Enemy("Green",defaultEnemy_icon2,280,260,[1,0],2,1)
         cat1.target = defaultEnemy2
         self.playergroup = pygame.sprite.GroupSingle(self.player)
-        self.enemies = pygame.sprite.Group(cat1,defaultEnemy2)
+        self.enemies = pygame.sprite.Group(cat1,defaultEnemy2,cat2,cat3)
+        self.borders = borders
+
 
         self.gameEventLoop()
 
