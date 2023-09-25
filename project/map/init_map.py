47411891b5f05cbcb0aaf46af2b747831cff099b
aogasd <144658261+aogasd@users.noreply.github.com>
2023-09-26 00:14:26 +0300

    generate a lot of creatures

--- project/map/init_map.py
+++ project/map/init_map.py
@@ -1,3 +1,6 @@
+from random import randint
+from data.settings import windowsizeX as X
+from data.settings import windowsizeY as Y
 from data.creature import Creature,Enemy, Pickup
 from data.player import Player
 from data.location import Location
@@ -17,15 +20,19 @@ defaultEnemy_icon2 = pygame.image.load("data/assets/TEST_Light_balls_tree1.png")
 player = Player("Marcos Petriades",marcos,50,50,(0,0))
 
 
-cat1 = Creature("Cat",cat1_ico,400,300,[1,0],1,1,None,{"walking":40,"standing":0})
-cat2 = Creature("Cat",cat1_ico,500,300,[1,0],2,1,None,{"walking":60,"standing":0})
-cat3 = Creature("Cat",cat1_ico,500,350,[1,0],3,1,None,{"walking":60,"standing":0})
-
-defaultEnemy2 = Enemy("Green",defaultEnemy_icon2,280,260,[1,0],2,1)
-defaultEnemy2.target = cat1
-
 playergroup = pygame.sprite.GroupSingle(player)
-friendlies = pygame.sprite.Group(cat1,cat2,cat3)
-enemies = pygame.sprite.Group(defaultEnemy2)
+collidables =pygame.sprite.Group()
+friendlies = pygame.sprite.Group()
+enemies = pygame.sprite.Group()
+
+cats = []
+trees = []
+for i in range(1,10):
+    friendlies.add(Creature("Cat",cat1_ico,randint(20,X-20),randint(20,Y-20),[1,0],randint(1,4),1,None,{"walking":40,"standing":0}))    #make 9
+    if i % 3 == 0 : enemies.add(Enemy("Green",defaultEnemy_icon2,randint(20,X-20),randint(20,Y-20),[1,0],2,1,awareness=50))             #make 3
+
 
 grouplist = [borders,friendlies,enemies,playergroup]
+for list in grouplist:
+    for sprite in list:
+        collidables.add(sprite)
