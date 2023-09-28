f4d162933fd29f5a1cc0ac146153f85e1b5d0dd4
aogasd <144658261+aogasd@users.noreply.github.com>
2023-09-28 16:54:31 +0300

    updated spawning

--- project/map/init_map.py
+++ project/map/init_map.py
@@ -1,8 +1,7 @@
+import math
 from random import randint
-from data.settings import windowsizeX as X
-from data.settings import windowsizeY as Y
-from data.settings import mapsizeX as mapX
-from data.settings import mapsizeY as mapY
+from data.settings import mapsizeX as X
+from data.settings import mapsizeY as Y
 from data.settings import show_hitboxes
 from data.location import Map_object,Decor
 from data.creature import Creature
@@ -15,67 +14,75 @@ from data.assets.images import *
 
 import pygame
 
+playerX,playerY = 930,450
 
-player = Player("Marcos Petriades",marcos_icon,int(X/2),int(Y/2),(0,0))
+player = Player("Marcos Goopyades",marcos_icon,playerX,playerY,(0,0))
 playergroup.add(player)
 
-       
-# def spawn(number,function):
-#     for i in range(number):
-#         function(i)
 
-# spawn(3,\
-#       itemgroup.add(Healing("Small Heal"+str(int(i/3)),testheart_icon,randint(20,mapX-20),randint(20,mapY-20)))
-#           )
-# spawn(5,\
-#     enemies.add(Enemy("Green"+str(int(i/3)),defaultEnemy_icon2,randint(20,mapX-20),randint(20,mapY-20),[1,0],2,3,150)))
 
-# spawn(10,\
-#     friendlies.add(Creature("Cat"+str(i),cat1_icon,randint(20,mapX-20),randint(20,mapY-20),[1,0],randint(2,4),2)))
 
-# spawn(10,\
-#     itemgroup.add(Currency("Pile-o-Gold"+str(i),testitem_icon,randint(20,mapX-20),randint(20,mapY-20))))
+'Get random coordinates for spawning each entry, first parameter is DISTANCE from player'
+def rand_coords(avoidplayer_dist=False,minX= 20, maxX= X-20 ,minY= 20 ,maxY= Y-20 ,):
+    newX = randint(minX,maxX)
+    newY = randint(minY,maxY)
 
-# spawn(10,\
-#     enemies.add(Spy("Spy"+str(i),spywillow_icon,randint(20,mapX-20),randint(20,mapY-20),[1,0],1,1,150)))
+    if avoidplayer_dist:
+        dist= math.dist((newX,newY),(playerX,playerY))
+        looplen = 0
+        while dist < avoidplayer_dist and looplen < 5:
+            newX = randint(minX,maxX)
+            newY = randint(minY,maxY)
+            dist= math.dist((newX,newY),(playerX,playerY))
+            looplen +=1
+    return newX,newY
 
 
 
 
 for i in range(3):
     ####################    name,                   image           pos_x           pos_y       dir,speed,health
-    itemgroup.add(Healing("Small Heal"+str(int(i/3)),testheart_icon,randint(20,mapX-20),randint(20,mapY-20)))
+    xx,yy = rand_coords(20)
+    itemgroup.add(Healing("Small Heal"+str(int(i/3)),testheart_icon,xx,yy))
     
 
-# Make objects, and add them to the Group    
+    
 for i in range(0,10):
-    ########################    name,      image    pos_x           pos_y           dir     speed   health
-    friendlies.add(Creature("Cat"+str(i),cat1_icon,randint(20,mapX-20),randint(20,mapY-20),[1,0],randint(2,4),2))           #make 9
-    itemgroup.add(Currency("Pile-o-Gold"+str(i),testitem_icon,randint(20,mapX-20),randint(20,mapY-20)))
-    enemies.add(Enemy("Green"+str(int(i/3)),defaultEnemy_icon2,randint(20,mapX-20),randint(20,mapY-20),[1,0],2,3,150))             #make 3
+    ###########                   name,      image pos_x,pos_y           dir     speed   health awareness
+    xx,yy = rand_coords(20)
+    friendlies.add(Creature("Cat"+str(i),cat1_icon,xx,yy,[1,0],randint(2,4),2))           
+    
+    xx,yy = rand_coords(20)
+    itemgroup.add(Currency("Pile-o-Gold"+str(i),testitem_icon,xx,yy))
+    
+    xx,yy = rand_coords(700)
+    enemies.add(Enemy("Green"+str(int(i/3)),defaultEnemy_icon2,xx,yy,[1,0],2,3,150))             
 
-# Make objects, and add them to the Group    
+ 
 for i in range(0,30):
-    ########################    name,      image    pos_x           pos_y           dir     speed   health
-    enemies.add(Spy("Spy"+str(i),spywillow_icon,randint(20,mapX-20),randint(20,mapY-20),[1,0],1,1,400))             #make 3
+    ########################    name,  image pos_x,pos_y, dir     speed   health,awareness
+    xx,yy = rand_coords(10)
+    enemies.add(Spy("Spy"+str(i),spywillow_icon,xx,yy,[1,0],1,1,400))             
+
+# def spawn(number,function):
+#     for i in range(number):
+#         function()
 
 
 # (startX,startY,sizeX,sizeY, starts from top left corner)
 
 bord_x0 = Map_object(-150,-150,pygame.image.load("data/assets/x-border-width.jpg"))
-bord_x100 = Map_object(-150,mapY+150,pygame.image.load("data/assets/x-border-width.jpg"))
+bord_x100 = Map_object(-150,Y+150,pygame.image.load("data/assets/x-border-width.jpg"))
 
 bord_y0 = Map_object(-150,-120,pygame.image.load("data/assets/y-border-height.jpg"))
-bord_y100 = Map_object(mapX+150,-120,pygame.image.load("data/assets/y-border-height.jpg"))
+bord_y100 = Map_object(X+150,-120,pygame.image.load("data/assets/y-border-height.jpg"))
 
 
 borders.add(bord_x0,bord_x100,bord_y0,bord_y100)
 
 for i in range(200):
-    
-    random_x = randint(50,mapX-50)
-    random_y = randint(50,mapY-50)
-    decor.add(Decor(random_x,random_y,willow_icon))
+    xx,yy = rand_coords(10)
+    decor.add(Decor(xx,yy,willow_icon))
 
 
 def add_to_camera():
