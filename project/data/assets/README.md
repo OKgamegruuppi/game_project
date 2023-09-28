3413d5c4c3932cf72485326bd470f7e74cf5fe4a
aogasd <144658261+aogasd@users.noreply.github.com>
2023-09-28 23:21:43 +0300

    added assets

--- project/data/assets/README.md
+++ project/data/assets/README.md
@@ -2,5 +2,6 @@
 2D Pixel Art Cat Sprites by Elthen's Pixel Art Shop https://elthen.itch.io/
 Blood Splatters + Attacks from https://code.google.com/archive/p/crawl-tiles/
 Heart by Ravenmore (http://dycha.net)
-heart healthbar opengameart.org/users/nexust
+heart healthbar based on https://opengameart.org/users/nexust https://opengameart.org/content/heart-health-bar
+haunted rees by https://opengameart.org/users/ettingrinder https://opengameart.org/content/haunted-forest-trees
 tbd
--- project/data/assets/images.py
+++ project/data/assets/images.py
@@ -2,13 +2,14 @@ import pygame
 
 # Creature Assets:
 marcos_icon = pygame.image.load("data/assets/slime_monster_spritesheet.png")
-cat1_icon = pygame.image.load("data/assets/Cat-sprite-stand.png")
-defaultEnemy_icon2 = pygame.image.load("data/assets/TEST_Light_balls_tree1.png")
+cat_icon = pygame.image.load("data/assets/quest-cat.png")
+defaultEnemy_icon2 = pygame.image.load("data/assets/Light_balls_tree.png")
 spywillow_icon = pygame.image.load("data/assets/Spy_Willow.png")
 
 # Item assets
 testitem_icon = pygame.image.load("data/assets/gold_pile.png")
 testheart_icon = pygame.image.load("data/assets/heart.png")
+quest_icon = pygame.image.load("data/assets/cat-collectible.png")
 
 # Effect assets
 player_attack_icon = pygame.image.load("data/assets/attack.png")
@@ -16,13 +17,36 @@ blood_red_icon = pygame.image.load("data/assets/blood_red1.png")
 small_heart_icon = pygame.image.load("data/assets/small_heart.png")
 
 # Map assets
-willow_icon = pygame.image.load('data/assets/edited_Willow.png')
+willow_icon = pygame.image.load('data/assets/safe_Willow.png')
 
 # UI assets 
-health_anim_icon = [pygame.image.load("data/assets/hp/0.png"), pygame.image.load("data/assets/hp/1.png"),
-              pygame.image.load("data/assets/hp/2.png"), pygame.image.load("data/assets/hp/3.png"),
-              pygame.image.load("data/assets/hp/4.png"), pygame.image.load("data/assets/hp/4.png"),
-              pygame.image.load("data/assets/hp/6.png")]
+health_anim_icon = [pygame.image.load("data/assets/hp/healthbar-00.png"), 	\
+                    pygame.image.load("data/assets/hp/healthbar-01.png"), 	\
+                    pygame.image.load("data/assets/hp/healthbar-02.png"), 	\
+                    pygame.image.load("data/assets/hp/healthbar-03.png"), 	\
+                    pygame.image.load("data/assets/hp/healthbar-04.png"), 	\
+                    pygame.image.load("data/assets/hp/healthbar-05.png"), 	\
+                    pygame.image.load("data/assets/hp/healthbar-06.png"), 	\
+                    pygame.image.load("data/assets/hp/healthbar-07.png"), 	\
+                    pygame.image.load("data/assets/hp/healthbar-08.png"), 	\
+                    pygame.image.load("data/assets/hp/healthbar-09.png"), 	\
+                    pygame.image.load("data/assets/hp/healthbar-10.png"), 	\
+                    pygame.image.load("data/assets/hp/healthbar-11.png"), 	\
+                    pygame.image.load("data/assets/hp/healthbar-12.png"), 	\
+                    pygame.image.load("data/assets/hp/healthbar-13.png"), 	\
+                    pygame.image.load("data/assets/hp/healthbar-14.png"), 	\
+                    pygame.image.load("data/assets/hp/healthbar-15.png"), 	\
+                    pygame.image.load("data/assets/hp/healthbar-16.png"), 	\
+                    pygame.image.load("data/assets/hp/healthbar-17.png"), 	\
+                    pygame.image.load("data/assets/hp/healthbar-18.png"), 	\
+                    pygame.image.load("data/assets/hp/healthbar-19.png"), 	\
+                    pygame.image.load("data/assets/hp/healthbar-20.png"), 	]
+
+                    
+            #          pygame.image.load("data/assets/hp/1.png"),
+            #   pygame.image.load("data/assets/hp/2.png"), pygame.image.load("data/assets/hp/3.png"),
+            #   pygame.image.load("data/assets/hp/4.png"), pygame.image.load("data/assets/hp/4.png"),
+            #   pygame.image.load("data/assets/hp/6.png")]
 
 # Function to split a spritesheet into a list of individual images
 def spritesheet(img,sprite_rows,sprite_cols):
