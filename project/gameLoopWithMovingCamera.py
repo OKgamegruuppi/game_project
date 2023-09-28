61d17c253196b8280185c127ec82aa815ef4c7ba
CoolCodeMan <81040054+CoolCodeMan@users.noreply.github.com>
2023-09-28 10:29:00 +0300

    Update gameLoopWithMovingCamera.py

--- project/gameLoopWithMovingCamera.py
+++ project/gameLoopWithMovingCamera.py
@@ -7,8 +7,7 @@ from map.camera import CameraGroup
 from data.init_groups import *
 from map.init_map import *
 from data.controls import game_event_observer
-from data.ui_elements import TextBox, Button
-
+from data.ui_elements import *
 
 class Mainloop():
     def __init__(self):
