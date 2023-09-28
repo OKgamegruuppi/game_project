a6d69c52bdbe94955c7785634ee186928b8f50ca
CoolCodeMan <81040054+CoolCodeMan@users.noreply.github.com>
2023-09-28 10:37:21 +0300

    Update gameLoopWithMovingCamera.py

--- project/gameLoopWithMovingCamera.py
+++ project/gameLoopWithMovingCamera.py
@@ -48,8 +48,8 @@ class Mainloop():
                 pausemenu_window = TextBox(windowsizeX/4,windowsizeY/4,windowsizeX/2,windowsizeY/2)
                 pause_menu_button_1 = Button(windowsizeX/3,windowsizeY/3,windowsizeX/3,windowsizeY/3,box_text="CONTINUE")
                 
-                pause_menu_button_1.update(screen=self.screen)
                 pausemenu_window.update(screen=self.screen)
+                pause_menu_button_1.update(screen=self.screen)
                 
                 pygame.display.flip()
                 pygame.display.update()
